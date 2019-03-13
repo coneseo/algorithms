4adc2d36a064a0dc1285a446511ee96e99427c72
coneseo <woon4754@gmail.com>
2019-03-13 21:57:37 +0900

    코딜리티 1번 binary gap

---
+++ src/main/java/basicAlgorithms/Testtt.java
@@ -0,0 +1,27 @@
+package basicAlgorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Testtt {
+    public int solution(int N) {
+        // write your code in Java SE 8
+        String binaryString = Integer.toBinaryString(N);
+
+        char[] binaryChar = binaryString.toCharArray();
+        int totalGapLength = 0;
+        int gapLength = 0;
+
+        for(int i=0;i<binaryChar.length;++i){
+            if(binaryChar[i] == '1'){
+                if(gapLength > totalGapLength) totalGapLength = gapLength;
+                gapLength = 0;
+            }else{
+                ++gapLength;
+            }
+        }
+
+        return totalGapLength;
+    }
+}
+
--- src/main/java/basicAlgorithms/backjun/ChickenHalfShort.java
+++ src/main/java/basicAlgorithms/backjun/ChickenHalfShort.java
@@ -15,7 +15,7 @@ public class ChickenHalfShort {
             //2*i(for문에서의 i변수)*c(반값치킨의 가격) + 양념치킨 수량-i랑 0이랑 비교해서 큰거 * 양치가격
             // + 후라이드 치킨 수량-i랑 0이랑 비교해서 큰거 *  후라이드 가격
             int price = 2*i*c + Math.max(0, x-i)*a + Math.max(0, y-i)*b;
-            // 
+            //
             if (ans > price) ans = price;
         }
         System.out.println(ans);
