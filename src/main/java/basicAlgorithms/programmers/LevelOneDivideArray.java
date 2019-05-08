af6bfc043586f863445c0b1e75d809da900461ab
coneseo <woon4754@gmail.com>
2019-05-09 00:04:19 +0900

    레벨 1 나누어 떨어지는 숫자 배열 수정

--- src/main/java/basicAlgorithms/programmers/LevelOneDivideArray.java
+++ src/main/java/basicAlgorithms/programmers/LevelOneDivideArray.java
@@ -2,21 +2,34 @@ package basicAlgorithms.programmers;
 import java.util.*;
 
 public class LevelOneDivideArray {
-    public int[] solution(int[] arr, int divisor) {
-        int[] answer = new int[arr.length];
-        int a = 0;
-        int count = 0;
+    public static int[] solution(int[] arr, int divisor) {
+        int[] answer;
+        List<Integer> temp = new ArrayList<>();
+
         for(int i=0; i<arr.length; i++){
             if(arr[i]%divisor == 0){
-                answer[a++] = arr[i];
-                count++;
+                temp.add(arr[i]) ;
             }
         }
 
-        if(count==0){
-            answer[a] = -1;
+        if(temp.size()==0){
+            temp.add(-1);
+        }
+        answer = new int[temp.size()];
+        for(int i=0; i<temp.size(); i++){
+            answer[i] = temp.get(i);
         }
         Arrays.sort(answer);
         return answer;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,9,7,10};
+        int divisor = 5;
+        int[] result = solution(arr,divisor);
+        for(int i = 0; i< result.length; i++){
+            System.out.println(result[i]+",");
+        }
+        System.out.println(result);
     }
 }
