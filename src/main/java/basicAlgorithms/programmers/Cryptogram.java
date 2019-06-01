ccef3a6bf8f3f80864a803e4f953b3bfe7f472db
coneseo <woon4754@gmail.com>
2019-06-01 17:13:17 +0900

    풀이 수정

--- src/main/java/basicAlgorithms/programmers/Cryptogram.java
+++ src/main/java/basicAlgorithms/programmers/Cryptogram.java
@@ -1,6 +1,9 @@
 package basicAlgorithms.programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /*
 암호문을 좋아하는 괴짜 개발자 브라운이 이번에는 중복 문자를 이용한 새로운 암호를 만들었습니다.
@@ -21,6 +24,18 @@ public class Cryptogram {
         //연속으로 같은 문자열이 있으면 제거하는데,
         //while문을 돌려서 중복되는 숫자가 없을 때까지라는 조건을 걸어주면 되겠다.
         String[] temp = null;
+        temp = cryptogram.split("");
+        List<String> list = Arrays.asList(temp);
+        boolean isGood = true;
+        int check = 0;
+        while(isGood) {
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i).equals(list.get(i + 1))) {
+                    list.remove(i);
+                    list.remove(i + 1);
+                }
+            }
+        }
 
 
         return answer;
