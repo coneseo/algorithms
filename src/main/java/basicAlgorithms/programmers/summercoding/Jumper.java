a42d23360e30bc6445f511c431182130b0dcd682
coneseo <woon4754@gmail.com>
2019-06-07 22:35:44 +0900

    점프와 순간이동 문제 풀이

--- src/main/java/basicAlgorithms/programmers/summercoding/Jumper.java
+++ src/main/java/basicAlgorithms/programmers/summercoding/Jumper.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class Jumper {
     public static void main(String[] args) {
-        System.out.println(Solution(5000));
+       // System.out.println(Solution(5000));
+        System.out.println(solution2(5000));
     }
     public static int Solution(int n){
 
@@ -20,5 +21,20 @@ public class Jumper {
         }
 
         return count;
+    }
+
+    public static int solution2(int n){
+        int sub = 1;
+        int ans = 0;
+        while(n != 0){
+            if(n % 2 == 1){
+                //n -= sub;
+                ans += 1;
+            }
+            n /= 2;
+        }
+        return ans;
+
+
     }
 }
