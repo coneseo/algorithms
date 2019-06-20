b5d4c38a3eda19d843a1cae157bfb89fe5f3cede
coneseo <woon4754@gmail.com>
2019-06-20 22:40:57 +0900

    20190620 탑코더 Cryptography 수정

--- src/main/java/topcoder/Cryptography.java
+++ src/main/java/topcoder/Cryptography.java
@@ -1,14 +1,22 @@
 package topcoder;
 
+import java.util.Arrays;
+
 public class Cryptography {
+    public static void main(String[] args){
+        Cryptography cryptography = new Cryptography();
+        int[] intArr = {1,3,2,1,1,3};
+        long result = cryptography.encrypt(intArr);
+        System.out.println(result);
+    }
     public long encrypt(int[] numbers) {
         long ans = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            long temp = 1;
-            for (int j = 0; j < numbers.length; j++) {
-                if (i == j) {
-                    temp *= (numbers[i] + 1);
-                } else {
+        for(int i=0; i<numbers.length; i++){
+            int temp = 1;
+            for(int j=0; j<numbers.length; j++){
+                if(i == j){
+                    temp *= (numbers[j]+1);
+                }else{
                     temp *= numbers[j];
                 }
             }
@@ -16,5 +24,19 @@ public class Cryptography {
         }
 
         return ans;
+    }
+    //간단한 수학적 지식이라는데
+    //이런 경우에는 가장 작은 수를 증가시키면 최대 곱이 나온다고 한다.
+    //간단하게 증명하면 +1 하면 곱의 증가율이 (n+1)/n이다.
+    //그래서 정렬을 한 다음에 가장 작은 수에 +1을 하고
+    //for문을 한번만 돌면 가장 큰 곱이 나온다. 
+    public long encryptWithSort(int[] numbers){
+        long ret = 1;
+        Arrays.sort(numbers);
+        numbers[0]++;
+        for(int i=0; i< numbers.length; i++){
+            ret *= numbers[i];
+        }
+        return ret;
     }
 }
