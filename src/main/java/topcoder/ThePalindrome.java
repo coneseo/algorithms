50b6bf873b3a9e627ebfec78f11126d0fdd0edad
coneseo <woon4754@gmail.com>
2019-10-07 23:54:17 +0900

    회문 풀이 추가

--- src/main/java/topcoder/ThePalindrome.java
+++ src/main/java/topcoder/ThePalindrome.java
@@ -2,14 +2,17 @@ package topcoder;
 
 public class ThePalindrome {
   public int find(String s){
+    //답의 문자 길이를 결정합니다.
     for(int i=s.length(); ; i++){
       boolean flag = true;
       for(int j=0;j<s.length(); j++){
+        //반대쪽에 문자가 존재하는 데 다를 경우 플래그를 변경합니다.
         if((i-j-1)<s.length() && s.charAt(j) != s.charAt(i-j-1)){
           flag = false;
           break;
         }
       }
+      //조건을 모두 만족하면 답을 리턴합니다.
       if(flag) return i;
     }
   }
