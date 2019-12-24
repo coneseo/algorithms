1cf4853cbfd31e368c584ae9cfa2c6176dd1331f
coneseo <woon4754@gmail.com>
2019-12-24 23:58:43 +0900

    주식 투자  시뮬레이션 변수 선언

--- src/main/java/topcoder/StockHistory.java
+++ src/main/java/topcoder/StockHistory.java
@@ -1,5 +1,16 @@
 package topcoder;
 
 public class StockHistory {
+  public int maximumEarnings(int initialInvestment, int monthlyContribution,
+      String[] stockPrices){
+    int money = initialInvestment;
+    int month = stockPrices.length;
+    int corp = stockPrices[0].split(" ").length;
 
+    int[][] prices = new int[month][corp];
+    double max =0, profit = 0;
+    double[] promotion = new double[ month - 1 ];
+    boolean[] buy = new boolean[month -1 ];
+
+  }
 }
