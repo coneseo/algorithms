50296020349c8650849497fa8d5168583f26c1da
coneseo <woon4754@gmail.com>
2019-12-25 23:47:40 +0900

    주식 투자 시물레이션 처리하기 쉽게 숫자 배열로 변환 추가

--- src/main/java/topcoder/StockHistory.java
+++ src/main/java/topcoder/StockHistory.java
@@ -9,7 +9,7 @@ public class StockHistory {
 
     int[][] prices = new int[month][corp];
     double max =0, profit = 0;
-    double[] promotion = new double[ month - 1 ];
+    double[] proportion = new double[ month - 1 ];
     boolean[] buy = new boolean[month -1 ];
 
     //처리하기 쉽게 숫자 배열로 변환
@@ -17,6 +17,28 @@ public class StockHistory {
       String[] s = stockPrices[i].split(" ");
       for(int j = 0; j < corp; j++){
         prices[i][j] = Integer.valueOf(s[j]);
+      }
+    }
+
+    //반복문을 역으로 돌려 거슬러 올라가면서
+    //각 월까지의 최대 증가율을 사야하는지 기록합니다.
+    for(int i = month - 2; 0 <= i; i--){
+      for(int j = 0; j < corp; j++){
+        double p = 1.0 * prices[month - 1][j] / prices[i][j] - 1;
+        if(0<p && max < p){
+          buy[i] = true;
+          max = p;
+          proportion[i] = p;
+
+        }
+      }
+    }
+
+    //처음부터 시작해서 사야하는 달이 오면 모든 자금을 투자
+    for(int i = 0; i <buy.length; i++){
+      if(buy[i]){
+        profit += money + proportion[i];
+        money = 0;
       }
     }
   }
