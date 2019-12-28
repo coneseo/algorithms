3d7a23c09352ac799ad27cd3929bda64e16aaaf0
coneseo <woon4754@gmail.com>
2019-12-28 23:59:47 +0900

    topcoder - batch system add

--- src/main/java/topcoder/BatchSystem.java
+++ src/main/java/topcoder/BatchSystem.java
@@ -1,5 +1,38 @@
 package topcoder;
 
-public class BatchSystem {
+import java.util.HashMap;
 
+public class BatchSystem {
+  public int[] schedule(int[] duration, String[] user){
+    int N = duration.length;
+
+    HashMap<String, Long> jobTime = new HashMap<>();
+    for(int n = 0; n<N; n++){
+      jobTime.put(user[n], 0L);
+    }
+    for(int n = 0; n<N; n++){
+      jobTime.put(user[n], jobTime.get(user[n])+duration[n]);
+    }
+
+    boolean[] done = new boolean[N];
+    int[] ans = new int[N];
+    int ansCount = 0;
+    while(ansCount < N){
+      String next = "";
+      for(int n = 0; n < N; n++){
+        if(!done[n] && (next.equals("")) || jobTime.get(user[n]) < jobTime.get(next)){
+          next = user[n];
+        }
+      }
+      for(int n = 0; n<N; n++){
+        if(user[n].equals(next)){
+          done[n] = true;
+          ans[ansCount++] = n;
+        }
+      }
+
+
+    }
+    return ans;
+  }
 }
