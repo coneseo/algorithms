46dd1b4d00b6d2ffb552cac3da1dbf2bad00d3be
coneseo <woon4754@gmail.com>
2020-01-29 23:55:22 +0900

    람다식 예제 1

---
+++ src/main/java/thisisjava/MyFunctionalInterfaceExample.java
@@ -0,0 +1,25 @@
+package thisisjava;
+
+public class MyFunctionalInterfaceExample {
+  public interface MyFunctionalInterface{
+    public void method();
+  }
+  public static void main(String[] args) {
+    MyFunctionalInterface fi;
+
+    fi = () ->{
+      String str = "method call1";
+      System.out.println(str);
+    };
+
+    fi.method();
+    fi = () -> {
+      System.out.println("method call2");
+    };
+    fi.method();
+
+    fi = () -> System.out.println("method call3");
+    fi.method();
+
+  }
+}
