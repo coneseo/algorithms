93abacceda7e5fa8407db5c55169c1c659616f4a
coneseo <woon4754@gmail.com>
2020-01-30 23:59:18 +0900

    람다식 예제 2 추가

--- src/main/java/thisisjava/MyFunctionalInterfaceExample2.java
+++ src/main/java/thisisjava/MyFunctionalInterfaceExample2.java
@@ -1,9 +1,28 @@
 package thisisjava;
 
 public class MyFunctionalInterfaceExample2 {
+  //매개 변수가 있고 리턴값이 없는 추상 메소드를 가진 함수적 인터페이스 선언
   @FunctionalInterface
   public interface MyFunctionalInterface{
     public void method(int x);
+  }
+
+  public static void main(String[] args) {
+    MyFunctionalInterface fi;
+
+    fi = (x) -> {
+      int result = x + 5;
+      System.out.println(result);
+    };
+    fi.method(2);
+
+    fi = (x) -> {
+      System.out.println(x+5);
+    };
+    fi.method(2);
+
+    fi = x-> System.out.println(x+5);
+    fi.method(2);
   }
 
 
