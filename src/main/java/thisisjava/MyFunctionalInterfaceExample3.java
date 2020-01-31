9546e0c4a60b306fcc988b0119517441aee7e219
coneseo <woon4754@gmail.com>
2020-01-31 23:46:13 +0900

    람다식 예제 2 추가 리턴값이 있는 경우

---
+++ src/main/java/thisisjava/MyFunctionalInterfaceExample3.java
@@ -0,0 +1,34 @@
+package thisisjava;
+
+import static java.lang.Integer.sum;
+
+public class MyFunctionalInterfaceExample3 {
+  //리턴값이 있는 추상 메소드를 가진 함수적 인터페이스
+  @FunctionalInterface
+  public interface MyFunctionalInterface{
+    public int method(int x, int y);
+  }
+
+  public static void main(String[] args) {
+    MyFunctionalInterface fi;
+
+    fi = (x,y) ->{
+      int result = x + y;
+      return result;
+    };
+
+    System.out.println(fi.method(2,5));
+
+    fi = (x,y) ->{return x + y;};
+
+    System.out.println(fi.method(2,5));
+
+    fi = (x,y) -> x+y;
+    System.out.println(fi.method(2,5));
+
+    fi = (x,y) -> sum(x,y);
+    System.out.println(fi.method(2,5));
+
+  }
+
+}
