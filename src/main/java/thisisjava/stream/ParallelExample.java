73d1bb72db631749ef06b4e3136d591dd35ad318
coneseo <woon4754@gmail.com>
2020-02-05 23:49:17 +0900

    ParallelExample 추가

---
+++ src/main/java/thisisjava/stream/ParallelExample.java
@@ -0,0 +1,23 @@
+package thisisjava.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ParallelExample {
+
+  public static void main(String[] args) {
+    List<String> list = Arrays.asList("hong","kin","lee");
+    //순차처리
+    Stream<String> stream = list.stream();
+    stream.forEach(ParallelExample :: print);
+    System.out.println();
+    //병렬처리
+    Stream<String> paralleStream = list.parallelStream();
+    paralleStream.forEach(ParallelExample :: print);
+
+  }
+  public static void print(String str){
+    System.out.println(str + " : "+Thread.currentThread().getName());
+  }
+}
