4ac2ca5b15cb74f9edda15f06e3c9b0ae696cde7
coneseo <woon4754@gmail.com>
2020-02-08 17:02:05 +0900

    로또번호 추첨기 추가

--- src/main/java/thisisjava/stream/DrawExample.java
+++ src/main/java/thisisjava/stream/DrawExample.java
@@ -1,6 +1,5 @@
 package thisisjava.stream;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -12,10 +11,21 @@ public class DrawExample {
 
     final List<String> participants = Arrays.asList("a","b","c","d","f","g","k");
 
-    new Random().ints(0,participants.size())
+//    new Random().ints(0,participants.size())
+//        .distinct()
+//        .limit(2)
+//        .mapToObj(participants::get)
+//        .forEach(System.out::println);
+
+
+
+    final List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+    ,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45);
+
+    new Random().ints(0, numbers.size())
         .distinct()
-        .limit(2)
-        .mapToObj(participants::get)
+        .limit(6)
+        .mapToObj(numbers::get)
         .forEach(System.out::println);
 
   }
