23830d561353faf4b96acad77261b7c8b1bfb94b
coneseo <woon4754@gmail.com>
2020-02-14 18:29:35 +0900

    Parentheses 추가

--- src/main/java/leetcode/Parentheses.java
+++ src/main/java/leetcode/Parentheses.java
@@ -1,6 +1,15 @@
 package leetcode;
 
+import java.util.Stack;
+
 public class Parentheses {
+
+  public static void main(String[] args) {
+    String s = "{{}}";
+    Parentheses parentheses = new Parentheses();
+    boolean valid = parentheses.isValid2(s);
+
+  }
   public boolean isValid(String s) {
     String[] array = s.split("");
     int length = array.length;
@@ -16,5 +25,24 @@ public class Parentheses {
       }
     }
     return flag;
+  }
+
+  public boolean isValid2(String s) {
+    Stack<Integer> st = new Stack<>();
+
+    for (char c : s.toCharArray()) {
+      switch (c) {
+        case '(':
+        case '{':
+        case '[':
+          st.push(Integer.valueOf(c));
+          break;
+        default:
+          if (st.empty() || Math.abs(st.pop()-c) > 2) //small cheat to use ASCI codes :)
+            return false;
+      }
+    }
+
+    return st.empty();
   }
 }
