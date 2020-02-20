2be9a37f3f08cec7ba37078f2b02701af1d33445
coneseo <woon4754@gmail.com>
2020-02-20 23:57:20 +0900

    filtering example 설명 추가

--- src/main/java/thisisjava/stream/FilteringExample.java
+++ src/main/java/thisisjava/stream/FilteringExample.java
@@ -1,5 +1,23 @@
 package thisisjava.stream;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import java.util.Arrays;
+import java.util.List;
+
 public class FilteringExample {
 
+  public static void main(String[] args) {
+    List<String> names = Arrays.asList("hong", "sin", "java", "sin", "min");
+    names.stream()
+        .distinct() //중복 제거
+        .forEach(n -> System.out.println(n));
+    System.out.println();
+
+    //필터링
+    names.stream().filter(n -> n.startsWith("s")).forEach(n -> System.out.println(n));
+    System.out.println();
+
+    //중복 제거 후 필터링
+    names.stream().distinct().filter(n -> n.startsWith("s")).forEach(n -> System.out.println(n));
+  }
 }
