695c2c7b9ffb9c95bb964b9332ca111339c936f7
coneseo <woon4754@gmail.com>
2020-02-22 20:43:39 +0900

    MapExample cnrk

---
+++ src/main/java/thisisjava/stream/MapExample.java
@@ -0,0 +1,14 @@
+package thisisjava.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import thisisjava.stream.FromCollectionExample.Student;
+
+public class MapExample {
+
+  public static void main(String[] args) {
+    List<Student> studentList = Arrays.asList(new Student("홍길동", 10), new Student("신용권", 20), new Student("유미선", 30));
+    studentList.stream().mapToInt(Student::getScore).forEach(score -> System.out.println(score));
+  }
+
+}
