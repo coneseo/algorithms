336937bc41e8cfbf04f57dc9334a171b74c0308b
coneseo <woon4754@gmail.com>
2020-03-10 23:59:47 +0900

    GroupingExample 남학생 매핑

--- src/main/java/thisisjava/stream/GroupingExample.java
+++ src/main/java/thisisjava/stream/GroupingExample.java
@@ -2,6 +2,8 @@ package thisisjava.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import thisisjava.stream.Student.City;
 import thisisjava.stream.Student.Sex;
 
@@ -14,5 +16,9 @@ public class GroupingExample {
         new Student("신용권", 10, Sex.MALE, City.Pusan),
         new Student("박수미", 6, Sex.FEMALE, City.Seoul)
     );
+    Map<Sex, List<Student>> mapBySex = totalList.stream().collect(Collectors.groupingBy(Student :: getSex));
+
+    System.out.println("[남학생]");
+    mapBySex.get(Sex.MALE).stream().forEach(s -> System.out.println(s.getName() + " "));
   }
 }
