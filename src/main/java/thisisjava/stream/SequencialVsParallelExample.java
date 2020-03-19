5ae74b3b375535dc78aec79ab9a1a5c01fb04721
coneseo <woon4754@gmail.com>
2020-03-19 23:53:52 +0900

    순차처리, 병렬처리

--- src/main/java/thisisjava/stream/SequencialVsParallelExample.java
+++ src/main/java/thisisjava/stream/SequencialVsParallelExample.java
@@ -1,5 +1,28 @@
 package thisisjava.stream;
 
-public class SequencialVsParallelExample {
+import java.util.List;
 
+public class SequencialVsParallelExample {
+  //요소처리
+  public static void work(int value) {
+    try{Thread.sleep(100);}catch(InterruptedException e){}
+  }
+
+  //순차처리
+  public static long testSequecial(List<Integer> list){
+    long start = System.nanoTime();
+    list.stream().forEach((a) -> work(a));
+    long end = System.nanoTime();
+    long runTime = end - start;
+    return runTime;
+  }
+
+  //병렬처리
+  public static long testParallel(List<Integer> list){
+    long start = System.nanoTime();
+    list.stream().parallel().forEach((a)-> work(a));
+    long end = System.nanoTime();
+    long runTime = end - start;
+    return runTime;
+  }
 }
