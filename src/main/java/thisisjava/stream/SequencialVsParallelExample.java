bd1a2277a0f993236b5f571e12f0c1e265feb714
coneseo <woon4754@gmail.com>
2020-03-20 23:37:54 +0900

    순차처리, 병렬처리 속도 비교

--- src/main/java/thisisjava/stream/SequencialVsParallelExample.java
+++ src/main/java/thisisjava/stream/SequencialVsParallelExample.java
@@ -1,5 +1,6 @@
 package thisisjava.stream;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SequencialVsParallelExample {
@@ -24,5 +25,20 @@ public class SequencialVsParallelExample {
     long end = System.nanoTime();
     long runTime = end - start;
     return runTime;
+  }
+
+  public static void main(String[] args) {
+    //소스 컬렉션
+    List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9);
+    //순차 스트림 처리 시간 구하기
+    long timeSequencial = testSequecial(list);
+    //병렬 스트림 처리 시간 구하기
+    long timeParallel = testParallel(list);
+
+    if(timeSequencial < timeParallel){
+      System.out.println("성능 테스트 결과 : 순차 처리가 더 빠름 ");
+    }else{
+      System.out.println("성능 테스트 겨로가 : 병렬 처리가 더 빠름 ");
+    }
   }
 }
