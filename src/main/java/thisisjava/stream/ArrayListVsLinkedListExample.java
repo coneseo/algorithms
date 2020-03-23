aab6c94f47454e5dbb3bbd69f036c939c0bec054
coneseo <woon4754@gmail.com>
2020-03-23 23:40:15 +0900

    ArrayListVsLinkedListExample 처리 시간 비교

--- src/main/java/thisisjava/stream/ArrayListVsLinkedListExample.java
+++ src/main/java/thisisjava/stream/ArrayListVsLinkedListExample.java
@@ -1,5 +1,7 @@
 package thisisjava.stream;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ArrayListVsLinkedListExample {
@@ -14,5 +16,29 @@ public class ArrayListVsLinkedListExample {
     long runTime = end - start;
     return runTime;
   }
+
+  public static void main(String... args){
+    //소스 컬렉션
+    List<Integer> arrayList = new ArrayList<>();
+    List<Integer> linkedList = new LinkedList<Integer>();
+    for(int i = 0; i < 1000000; i++){
+      arrayList.add(i);
+      linkedList.add(i);
+    }
+    //워밍업
+    long arrayListListParallel = testParallel(arrayList);
+    long linkedListParallel = testParallel(linkedList);
+
+    //병렬 스트림 처리 시간 구하기
+    arrayListListParallel = testParallel(arrayList);
+    linkedListParallel = testParallel(linkedList);
+
+    if(arrayListListParallel < linkedListParallel){
+      System.out.println("성능 테스트 결과 : ArrayList 처리가 더 빠름 ");
+    }else{
+      System.out.println("성능 테스트 결과 : LinkedList 처리가 더 빠름 ");
+    }
+  }
+
 
 }
