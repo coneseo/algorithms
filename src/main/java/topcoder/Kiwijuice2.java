86155882545971e31f12225058d595f2f855bd02
coneseo <woon4754@gmail.com>
2020-03-26 23:56:30 +0900

    topcoder#1 kiwijuice 문제 풀이

--- src/main/java/topcoder/Kiwijuice2.java
+++ src/main/java/topcoder/Kiwijuice2.java
@@ -8,4 +8,55 @@ public class Kiwijuice2 {
   병 fromId[i] 가 비어있거나 병 toId[i]가 꽉 차 있는 순간, 타로는 더 이상 키위 주스를 넣지 않는다.
   N개의 요소를 가진 정수 배열 int[]를 리턴해주세요. 배열의 i번째 요소는 모든 주스를 쏟는 작업이 완료되고 i번째 병에 남아있는 키위 주스의 양입니다.
  */
+
+  public int[] thePouring(int[] capacities, int[] bottles, int[] fromId, int[] toId){
+    int[] result = new int[bottles.length];
+    for(int i = 0; i < fromId.length; i++){
+      int from = fromId[i];
+      int to = toId[i];
+      int fromAmount = bottles[from];
+      int toAmount = bottles[to];
+      int sum = fromAmount + toAmount;
+      int capacityOfto = capacities[to];
+      if(sum > capacityOfto){
+        bottles[from] = sum - capacityOfto;
+        bottles[to] = capacityOfto;
+      }else{
+        bottles[from] = 0;
+        bottles[to] = sum;
+      }
+    }
+    return bottles;
+  }
+
+  public int[] thePouring2(int[] capacities, int[] bottles, int[] fromId, int[] toId){
+    for(int i = 0; i < fromId.length; i++){
+      int from = fromId[i];
+      int to = toId[i];
+      int val = Math.min(bottles[from], capacities[to] - bottles[to]);
+      bottles[from] -= val;
+      bottles[to] += val;
+    }
+    return bottles;
+  }
+
+  public static void main(String[] args) {
+    Kiwijuice2 kiwijuice2 = new Kiwijuice2();
+//    int[] capacities = {20, 20};
+//    int[] bottles = {5, 8};
+//    int[] fromId = {0};
+//    int[] toId = {1};
+//    int[] capacities = {30, 20, 10};
+//    int[] bottles = {10, 5, 5};
+//    int[] fromId = {0, 1, 2};
+//    int[] toId = {1, 2, 0};
+    int[] capacities = {14, 35, 86, 58, 25, 62};
+    int[] bottles = {6, 34, 27, 38, 9, 60};
+    int[] fromId = {1, 2, 4, 5, 3, 3, 1, 0};
+    int[] toId = {0, 1, 2, 4, 2, 5, 3, 1};
+    int[] ints = kiwijuice2.thePouring2(capacities, bottles, fromId, toId);
+    for(int i : ints){
+      System.out.println(i);
+    }
+  }
 }
