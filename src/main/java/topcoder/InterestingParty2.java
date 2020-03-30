ec63ff9e944f8bcf7909df871ffb3e3bf4773a12
coneseo <woon4754@gmail.com>
2020-03-30 23:53:48 +0900

    topcoder#2 InterestingParty2 풀이 추가

--- src/main/java/topcoder/InterestingParty2.java
+++ src/main/java/topcoder/InterestingParty2.java
@@ -1,5 +1,7 @@
 package topcoder;
 
+import java.util.HashMap;
+
 public class InterestingParty2 {
   /*
   다재다능한 화이트 씨가 있다. 그에게는 친구들이 있다. 각 친구들은 2가지 주제에만 관심이 있고 다른 주제로 이야기하는 것을 싫어한다.
@@ -20,8 +22,29 @@ public class InterestingParty2 {
         if(second[i].equals(first[j])) s++;
         if(second[i].equals(second[j])) s++;
       }
-      check = Math.max()
+      check = Math.max(f, check);
+      check = Math.max(s, check);
     }
+    return check;
+  }
+
+  public int bestInvitation2(String[] first, String[] second){
+    HashMap<String, Integer> dic = new HashMap<>();
+    for(int i=0; i<first.length; i++){
+      dic.put(first[i], 0);
+      dic.put(second[i], 0);
+    }
+
+    for(int i=0; i<first.length; i++){
+      dic.put(first[i], dic.get(first[i])+1);
+      dic.put(second[i], dic.get(second[i])+1);
+    }
+
+    int ans = 0;
+    for(String key : dic.keySet()){
+      ans = Math.max(ans, dic.get(key));
+    }
+    return ans;
   }
 
   /*
