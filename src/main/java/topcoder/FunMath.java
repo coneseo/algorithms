884e10d084174180fd52a9f803b0491ee025cc45
coneseo <woon4754@gmail.com>
2020-04-02 23:59:25 +0900

    topcoder#3 재미있는 수학 추가

--- src/main/java/topcoder/FunMath.java
+++ src/main/java/topcoder/FunMath.java
@@ -1,5 +1,14 @@
 package topcoder;
 
+import java.util.ArrayList;
+
 public class FunMath {
+  public int[] digits(int base){
+    ArrayList<Integer>  v = new ArrayList<>();
+
+    for(int n = 2; n < base; n++){
+      boolean ok = false;
+    }
+  }
 
 }
