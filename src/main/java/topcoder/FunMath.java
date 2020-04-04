c9e370eb8c26fb74f2b42426fa26fdd3262f6231
coneseo <woon4754@gmail.com>
2020-04-04 17:33:51 +0900

    topcoder#3 재미있는 수학 추가

--- src/main/java/topcoder/FunMath.java
+++ src/main/java/topcoder/FunMath.java
@@ -1,6 +1,7 @@
 package topcoder;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class FunMath {
   public int[] digits(int base){
@@ -28,6 +29,17 @@ public class FunMath {
 
     return ans;
 
+  }
+
+  public int[] digits2(int base){
+    Vector<Integer> v = new Vector<>();
+    for(int i = 2 ; i < base; i++){
+      if((base -1) % i == 0) v.add(i);
+    }
+    int[] ans = new int[v.size()];
+    for(int i = 0; i<v.size(); i++) ans[i] = v.get(i);
+
+    return ans;
   }
 
 }
