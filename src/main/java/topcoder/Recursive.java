45510d6bf41c68fec50854df452aae37d030f00d
coneseo <woon4754@gmail.com>
2020-04-18 16:59:23 +0900

    #topcoder 설명 코드라 오류나는것 주석 처리(recursive)

--- src/main/java/topcoder/Recursive.java
+++ src/main/java/topcoder/Recursive.java
@@ -5,15 +5,15 @@ public class Recursive {
     if(a <= 1) return 1;
     return fib(a - 1)+fib(a -2);
   }
-  int dfs(int now){
-    if(현재 상태가 now가 끝나는 조건){
-      return 현재 상태 now의 값;
-    }
-    int ret = -1;
-    for(int i = 0; i< 다음 상태 개수; i++){
-      int next = i번째 다음 상태;
-      if(next가 조건을 만족하는 경우) ret = Math.max(dfs(next), ret);
-    }
-    return net;
-  }
+//  int dfs(int now){
+////    if(현재 상태가 now가 끝나는 조건){
+////      return 현재 상태 now의 값;
+////    }
+////    int ret = -1;
+////    for(int i = 0; i< 다음 상태 개수; i++){
+////      int next = i번째 다음 상태;
+////      if(next가 조건을 만족하는 경우) ret = Math.max(dfs(next), ret);
+////    }
+////    return net;
+//  }
 }
