f28366ed33bf3dbe6ee545d14efea57cfe6b405a
coneseo <woon4754@gmail.com>
2020-04-19 23:56:04 +0900

    programmers stack/queue Top

---
+++ src/main/java/programmers/StackQueue/Top.java
@@ -0,0 +1,20 @@
+package programmers.StackQueue;
+
+public class Top {
+  public int[] solution(int[] heights) {
+    int len = heights.length;
+    int[] answer = new int[len];
+
+    for(int i = len-1; i > 0; i--){
+      for(int j = i-1; j > 0; j--){
+        if(heights[j] < heights[i]){
+          answer[i] = j+1;
+          break;
+        }
+      }
+    }
+    answer[0] = 0;
+
+    return answer;
+  }
+}
